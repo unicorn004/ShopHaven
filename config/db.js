0e68a167163dab538c4d5535caed3e280101443b
Tanmay Sinkar <tanmaysinkar2021@gmail.com>
2024-10-26 16:45:18 +0530

    mongodb fix

--- config/db.js
+++ config/db.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGOURL).then(function(){
-    console.log('Connected to MongoDB');
-});
+mongoose.connect(process.env.MONGOURL)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch(err => {
+        console.error('MongoDB connection error:', err);
+    });
 
 module.exports = mongoose.connection;
